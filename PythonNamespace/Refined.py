34f3c240a925eeec2020992a71dfa3bc711c2e04
Jonas McClure <jmcclure62@ivytech.edu>
2019-10-15 13:29:47 -0400

    Updated

--- PythonNamespace/Refined.py
+++ PythonNamespace/Refined.py
@@ -1,1 +1,12 @@
+# From https://hackersandslackers.com/scraping-urls-with-beautifulsoup/
+# LOOK https://stackoverflow.com/questions/46987241/using-beautifulsoup-where-authentication-is-required
 
+import requests
+from bs4 import BeautifulSoup
+
+# Set Headers
+
+url = "https://google.com"
+req = requests.get(url)
+soup = BeautifulSoup(req.content, 'html.parser')
+print(soup.prettify())
